f1a8f74ef78b1ae3954945ce5e1d2b9ba4fff827
Tim Lu <tim.lu@ourlog.ca>
2016-08-03 21:08:21 +0000

    Changed exercise 3 to use validation method instead.

--- ar-exercises/lib/store.rb
+++ ar-exercises/lib/store.rb
@@ -1,18 +1,17 @@
-class StoreValidator < ActiveModel::Validator
-  def validate(store)
-    unless store.mens_apparel || store.womens_apparel
-      store.errors[:mens_apparel] << "One of mens_apparel or womens_apparel must be true"
-      store.errors[:womens_apparel] << "One of mens_apparel or womens_apparel must be true"
-    end
-  end
-end
+# class StoreValidator < ActiveModel::Validator
+#   def validate(store)
+#     unless store.mens_apparel || store.womens_apparel
+#       store.errors[:mens_apparel] << "One of mens_apparel or womens_apparel must be true"
+#       store.errors[:womens_apparel] << "One of mens_apparel or womens_apparel must be true"
+#     end
+#   end
+# end
 
 class Store < ActiveRecord::Base
 
   include ActiveModel::Validations
 
   has_many :employees
-  validates_with StoreValidator
   validates :name,
     presence: true,
     length: {
@@ -24,5 +23,13 @@ class Store < ActiveRecord::Base
       only_integer: true,
       greater_than: 0
     }
+  validate :must_carry_some_apparel
+
+  def must_carry_some_apparel
+    if !mens_apparel && !womens_apparel
+      errors.add(:mens_apparel, "Must be some apparel at the store.")
+      errors.add(:womens_apparel, "Must be some apparel at the store.")
+    end
+  end
 
 end
